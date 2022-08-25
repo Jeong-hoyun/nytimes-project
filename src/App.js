5d63c44cd2505d0218b0ef5ce0a548869df71cc4
jeong-hoyun <ghdbs125@gmail.com>
2022-08-25 15:31:16 +0900

    fect : add routing
    / 에서는 기사 검색용 컴포넌트 index.js
    api를 이용하기 위한 api.js
    clip 용 clip.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import { BrowserRouter,Route,Routes } from "react-router-dom";
+import Index from './news/index';
+import Clip from './clip/clip';
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Index />} />
+      <Route path="/clip/" element={<Clip />} />
+      <Route path="/*" element={<Index />} />
+    </Routes>
+  </BrowserRouter>
   );
 }
 
