70fceb808c7b42b1deee6e1214fcd849857b536d
1sol2sol <17gksthf@gmail.com>
2022-08-26 15:46:30 +0900

    feat: Get api for rendering news

--- src/api/index.js
+++ src/api/index.js
@@ -1,3 +1,2 @@
 export const API_KEY = "Ali6AtsGeFJP74mqOcxADqhoVC7StV5X";
 export const getUrl = ({ q, page }) => `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${q}&page=${page}&sort=newest&api-key=${API_KEY}`;
-
