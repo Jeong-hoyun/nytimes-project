5b99b4d99bfbac5084ba3a05c586b3f3d7e9bd0d
1sol2sol <17gksthf@gmail.com>
2022-08-29 16:37:21 +0900

    feat: Add Infinite Scroll func

--- src/api/index.js
+++ src/api/index.js
@@ -1,2 +1,4 @@
-export const API_KEY = "Ali6AtsGeFJP74mqOcxADqhoVC7StV5X";
+export const API_KEY = "6tfNVzyLzZGfmEbE9s6T8imqnbkUAylZ";
 export const getUrl = ({ q, page }) => `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${q}&page=${page}&sort=newest&api-key=${API_KEY}`;
+
+// Ali6AtsGeFJP74mqOcxADqhoVC7StV5X
